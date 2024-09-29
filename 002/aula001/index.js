315372f3b895b243ee5ea671ccc57b4f412acced
valdemarnanon <139665106+valdemarnanon@users.noreply.github.com>
2024-09-29 17:47:26 -0300

    ++

---
+++ 002/aula001/index.js
@@ -0,0 +1,14 @@
+// void
+// retorno
+
+function calcularAreaTerreno(largura, comprimento) {
+    const area = largura * comprimento;
+
+    return area
+}
+
+const a1 = calcularAreaTerreno(15, 25);
+const a2 = calcularAreaTerreno(10, 20);
+
+console.log(a1);
+console.log(a2);
